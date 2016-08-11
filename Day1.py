d99c2decf71ec911780708e34f842347d60cb7ae
Vivek Rana <vickyvksr97@yahoo.in>
2016-08-11 18:40:12 +0530

    Completed Day1 of 30 days of code

---
+++ Day1.py
@@ -0,0 +1,22 @@
+"""
+Usage :	Just copy paste the content of the script in the iPython interperter,
+		or just call the script. The scripts written are only for
+		study purpose.
+
+Author:	Vivek Rana
+
+Date:	11-Aug-2016
+
+About Script:
+		This is a basic "Hello World" script.
+		For more info check : https://www.hackerrank.com/challenges/30-hello-world
+"""
+
+def main():
+	inputString = input("Enter a String:")
+	print(inputString)
+	print("Hello World!")
+	return
+	
+if __name__ == '__main__':
+	main()
