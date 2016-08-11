1c65ea85c5e15a7fcf8550f9d8d8dae337133ba2
Vivek Rana <vickyvksr97@yahoo.in>
2016-08-11 18:48:48 +0530

    Day2 completed

---
+++ Day2.py
@@ -0,0 +1,28 @@
+"""
+Usage :	Just copy paste the content of the script in the iPython interperter,
+		or just call the script. The scripts written are only for
+		study purpose.
+
+Author:	Vivek Rana
+
+Date:	11-Aug-2016
+
+About Script:
+		Write some thing about script here.
+		https://www.hackerrank.com/challenges/30-data-types
+"""
+
+def main():
+	i = 4
+	d = 4.0
+	s = 'HackerRank '
+	myInt = int(input())
+	myFloat = float(input())
+	myString = input()
+	print(i + myInt)
+	print(d + myFloat)
+	print(s + myString)
+	return 
+
+if __name__ == '__main__':
+	main()
